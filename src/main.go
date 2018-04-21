97d177012c42ac5d59da9e67ae1936df58772c19
Salvador Guzman <guzmansalv@gmail.com>
2018-04-21 13:06:54 -0700

    scrape links

--- src/main.go
+++ src/main.go
@@ -80,47 +80,48 @@ func main() {
     }
 
     links := make(chan []string, limit)
-    linkThread := func(j int) {
-        for i := j; i <= limit; i += cores {
-            url := strings.Join([]string{baseLink, strconv.Itoa(i)}, "")
-            bod := get(url)
-            root, err := html.Parse(strings.NewReader(bod))
-            if err != nil {
-                panic(err)
-            }
-
-            article := scrape.FindAll(root, scrape.ByTag(atom.Article))
-
-            miniLinks := make([]string, 0)
-            for as := range article {
-                a := article[as]
-                link, err := scrape.Find(a, scrape.ByTag(atom.A))
-                if !err {
+    allLinks := make([]string, 0)
+    {
+        linkThread := func(j int) {
+            for i := j; i <= limit; i += cores {
+                url := strings.Join([]string{baseLink, strconv.Itoa(i)}, "")
+                bod := get(url)
+                root, err := html.Parse(strings.NewReader(bod))
+                if err != nil {
                     panic(err)
                 }
 
-                href := scrape.Attr(link, "href")
-                trimmed := strings.TrimPrefix(href, "https://it-eb.com")
-                miniLinks = append(miniLinks, trimmed)
+                article := scrape.FindAll(root, scrape.ByTag(atom.Article))
+
+                miniLinks := make([]string, 0)
+                for as := range article {
+                    a := article[as]
+                    link, err := scrape.Find(a, scrape.ByTag(atom.A))
+                    if !err {
+                        panic(err)
+                    }
+
+                    href := scrape.Attr(link, "href")
+                    trimmed := strings.TrimPrefix(href, "https://it-eb.com")
+                    miniLinks = append(miniLinks, trimmed)
+                }
+
+                links <- miniLinks
             }
+        }
+        for j := 1; j <= cores; j += 1 {
+            go linkThread(j)
+        }
 
-            links <- miniLinks
+
+        for i := 1; i < limit; i += 1 {
+            receivedLinks := <- links
+            for k := range receivedLinks {
+                str := receivedLinks[k]
+                allLinks = append(allLinks, str)
+            }
         }
     }
-    for j := 1; j <= cores; j += 1 {
-        go linkThread(j)
-    }
-
-    allLinks := make([]string, 0)
-    for i := 1; i < limit; i += 1 {
-        receivedLinks := <- links
-        for k := range receivedLinks {
-            str := receivedLinks[k]
-            allLinks = append(allLinks, str)
-        }
-    }
-
-    fmt.Printf("Total elements is %d\n", len(allLinks))
 
     type BookDetails struct {
         title string
@@ -131,79 +132,104 @@ func main() {
     }
 
     books := make(chan BookDetails, 1000)
-    bookThread := func(i int) {
-        for j := i; j < len(allLinks); j += cores {
-            path := allLinks[j]
-            url := strings.Join([]string{baseBook, path}, "")
-            htmlBody := get(url)
-            root, err := html.Parse(strings.NewReader(htmlBody))
-            if err != nil {
-                panic(err)
-            }
+    allBooks := make([]BookDetails, 0)
+    {
+        bookThread := func(i int) {
+            for j := i; j < len(allLinks); j += cores {
+                path := allLinks[j]
+                url := strings.Join([]string{baseBook, path}, "")
+                htmlBody := get(url)
+                root, err := html.Parse(strings.NewReader(htmlBody))
+                if err != nil {
+                    panic(err)
+                }
 
-            title, ok := scrape.Find(root, scrape.ByClass("post-title"))
-            if !ok {
-                panic(ok)
-            }
-
-            img, ok := scrape.Find(root, scrape.ByClass("book-cover"))
-            if !ok {
-                panic(ok)
-            }
-
-            imgText := scrape.Attr(img.FirstChild, "src")
-            imgTextTrimmed := strings.TrimPrefix(imgText, "https://it-eb.com")
-
-            desc, ok := scrape.Find(root, scrape.ByClass("entry-inner"))
-            if !ok {
-                panic(ok)
-            }
-
-            idMatch := func (node *html.Node) bool {
-                return node.DataAtom == atom.Input && scrape.Attr(node, "type") == "hidden" && scrape.Attr(node, "name") == "comment_post_ID"
-            }
-
-            id, ok := scrape.Find(root, idMatch)
-            if !ok {
-                panic(ok)
-            }
-
-            idVal := scrape.Attr(id, "value")
-
-            bookdetailsMap := make(map[string]string)
-            {
-                bookdetails, ok := scrape.Find(root, scrape.ByClass("book-details"))
+                title, ok := scrape.Find(root, scrape.ByClass("post-title"))
                 if !ok {
                     panic(ok)
                 }
 
-                bookdetailsKeys := scrape.FindAll(bookdetails, scrape.ByTag(atom.Span))
-                bookdetailsVals := scrape.FindAll(bookdetails, scrape.ByTag(atom.Li))
-                for i := range bookdetailsKeys {
-                    key := bookdetailsKeys[i]
-                    val := bookdetailsVals[i]
-
-                    keyStr := scrape.Text(key)
-                    valStr := scrape.Text(val)
-
-                    trimKey := strings.ToLower(strings.TrimSuffix(keyStr, ":"))
-                    trimVal := strings.TrimPrefix(valStr, keyStr)
-                    bookdetailsMap[trimKey] = trimVal
+                img, ok := scrape.Find(root, scrape.ByClass("book-cover"))
+                if !ok {
+                    panic(ok)
                 }
-            }
 
-            books <- BookDetails{scrape.Text(title), imgTextTrimmed, idVal, bookdetailsMap, scrape.Text(desc)}
+                imgText := scrape.Attr(img.FirstChild, "src")
+                imgTextTrimmed := strings.TrimPrefix(imgText, "https://it-eb.com")
+
+                desc, ok := scrape.Find(root, scrape.ByClass("entry-inner"))
+                if !ok {
+                    panic(ok)
+                }
+
+                idMatch := func (node *html.Node) bool {
+                    return node.DataAtom == atom.Input && scrape.Attr(node, "type") == "hidden" && scrape.Attr(node, "name") == "comment_post_ID"
+                }
+
+                id, ok := scrape.Find(root, idMatch)
+                if !ok {
+                    panic(ok)
+                }
+
+                idVal := scrape.Attr(id, "value")
+
+                bookdetailsMap := make(map[string]string)
+                {
+                    bookdetails, ok := scrape.Find(root, scrape.ByClass("book-details"))
+                    if !ok {
+                        panic(ok)
+                    }
+
+                    bookdetailsKeys := scrape.FindAll(bookdetails, scrape.ByTag(atom.Span))
+                    bookdetailsVals := scrape.FindAll(bookdetails, scrape.ByTag(atom.Li))
+                    for i := range bookdetailsKeys {
+                        key := bookdetailsKeys[i]
+                        val := bookdetailsVals[i]
+
+                        keyStr := scrape.Text(key)
+                        valStr := scrape.Text(val)
+
+                        trimKey := strings.ToLower(strings.TrimSuffix(keyStr, ":"))
+                        trimVal := strings.TrimPrefix(valStr, keyStr)
+                        bookdetailsMap[trimKey] = trimVal
+                    }
+                }
+
+                books <- BookDetails{scrape.Text(title), imgTextTrimmed, idVal, bookdetailsMap, scrape.Text(desc)}
+            }
+        }
+
+
+        for i := 1; i <= cores; i += 1 {
+            go bookThread(i)
+        }
+
+        for i := 1; i < len(allLinks); i += 1 {
+            receivedBook := <- books
+            allBooks = append(allBooks, receivedBook)
         }
     }
 
-    for i := 1; i <= cores; i += 1 {
-        go bookThread(i)
-    }
+    hosts := make(chan string, 1000)
+    hostsLinks := make([]string, 0)
+    {
+        hostThread := func(i int) {
+            for j := i; j < len(allBooks); j += cores {
+                pathId := allBooks[j].id
+                url := strings.Join([]string{"http://23.95.221.108/download.php?id=", pathId}, "")
+                htmlBody := get(url)
+                hosts <- htmlBody
+            }
+        }
 
-    allBooks := make([]BookDetails, 0)
-    for i := 1; i < len(allLinks); i += 1 {
-        receivedBook := <- books
-        fmt.Println(receivedBook)
-        allBooks = append(allBooks, receivedBook)
+        for i := 1; i <= cores; i += 1 {
+            go hostThread(i)
+        }
+
+        for i := 1; i < len(allBooks); i += 1 {
+            receivedHostLink := <- hosts
+            fmt.Println(receivedHostLink)
+            hostsLinks = append(hostsLinks, receivedHostLink)
+        }
     }
 }
